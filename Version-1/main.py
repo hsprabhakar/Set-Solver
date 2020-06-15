7b0cb0bf1d74517fec74c124e864a096b0e81956
Saurab Harish Prabhakar <49376623+hsprabhakar@users.noreply.github.com>
2020-06-14 23:09:11 -0600

    Create main.py

---
+++ Version-1/main.py
@@ -0,0 +1,23 @@
+import art
+from game import Game
+
+def main():
+    finished = False
+    print('WELCOME TO FAIQUESUCKSATSETSOTHESEAREHACKS.EXE')
+    art.tprint('OwO', font='random')
+
+    while not finished:
+        num_cards = input('Number of cards on screen right now: ')
+        print('Number of Cards on screen: ', num_cards)
+
+        game = Game(num_cards)
+        game.receive_attribute()
+        game.iterator()
+
+        quit_program = input('Are there more cards? ')
+        if quit_program == 'n' or quit_program == 'N':
+            art.tprint('Git Gud', font='random')
+            finished = True
+
+
+main()
